909ea8ebdc6b971dca132b70e0fca2ebaf7a4b3d
atif shaikh <shaikhatif456a@gmail.com>
2024-05-05 14:26:16 +0530

    create and run server

---
+++ api/index.js
@@ -0,0 +1,6 @@
+import express from  'express'
+const app= express();
+
+app.listen(3000,()=>{
+    console.log('server listenin on port 300')
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,29 @@
-
+import { BrowserRouter  ,Routes , Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx' 
+import Profile from './pages/Profile.jsx'
+import About from './pages/About.jsx' 
+import Header from './Components/Header.jsx'
 
 function App() {
 
 
   return (
-    <>
-     <h1>app components
 
-     </h1>
-    </>
+   <>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />  
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+   </>
   )
 }
 
